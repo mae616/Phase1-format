14001d719f4be3a9774dfe051f6873730c4d101a
mae616 <oya2.mi@hotmail.co.jp>
2021-09-06 08:10:13 +0900

    基礎 値

---
+++ js-easy/js-basic/main.js
@@ -0,0 +1,23 @@
+// こんにちは、世界！
+// console.log('Hello, World!')
+
+// 値
+
+// 数値
+// console.log(100)
+// console.log(1.5)
+// console.log(-1)
+
+// 文字列
+// console.log('Hello!')
+// console.log('ありがとう!')
+
+// 配列
+// console.log([1, 2, 3, 4, 5])
+
+// 真偽値(しんぎち)
+// console.log(true)
+// console.log(false)
+
+// オブジェクト
+// console.log({ one: 1, two: 2 })
