e51695ef2e7f4623b609a8c9d76ded195c8140f7
mae616 <oya2.mi@hotmail.co.jp>
2021-09-06 08:14:22 +0900

    条件分岐

--- js-easy/js-basic/main.js
+++ js-easy/js-basic/main.js
@@ -1,42 +1,13 @@
-// 配列の操作
+const num = Math.random() // Math.random() => 0から1までのランダムな数を返してくれるプログラム
 
-// const nengou = ['明治', '大正', '昭和', '平成']
-// console.log(nengou)
+if (num >= 0.5) { // numが0.5以上である場合
+    console.log('大きめ')
+} else {
+    console.log('小さめ')
+}
 
-// // 要素の追加
-// nengou.push('令和')
+console.log(`数: ${num}`)
 
-// console.log(nengou) // => ["明治", "大正", "昭和", "平成", "令和"]
-
-// nengou.splice(3, 1) // 配列.splice(削除したい要素のインデックス, 削除したい要素の個数)
-
-// console.log(nengou) // => ["明治", "大正", "昭和", "令和"]
-
-// nengou.splice(2, 2) // 配列.splice(削除したい要素のインデックス, 削除したい要素の個数)
-
-// console.log(nengou) // =>  ["明治", "大正"]
-// -------------------------------
-
-const nengou = ['明治', '大正', '昭和', '平成']
-console.log(nengou)
-
-// 要素の変更
-nengou[2] = 'showa'
-
-console.log(nengou) // ["明治", "大正", "showa", "平成"]
-
-
-
-
-
-
-// -------------------------------
-// 自分で勝手にやったの
-// const nengou = ['明治', '大正', '昭和', '平成']
-// console.log(nengou)
-
-// // 要素の追加（要素数が返る）
-// const nengou2 = nengou.push('令和')
-
-// console.log(nengou) // =>追加されたの
-// console.log(nengou2) // =>追加された後の要素数
+//-------------------
+// 実行したいのが一行の場合こういう書き方もできる
+if (num >= 0.5) console.log('大きめ')
