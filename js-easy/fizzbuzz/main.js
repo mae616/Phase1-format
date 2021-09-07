77c1e82b49a24b4e972fb78e51625610fe016ecf
mae616 <oya2.mi@hotmail.co.jp>
2021-09-07 17:19:25 +0900

    FizzBuzz チャレンジ問題

--- js-easy/fizzbuzz/main.js
+++ js-easy/fizzbuzz/main.js
@@ -1,20 +1,28 @@
-for (let i = 1; i <= 20; i++) {
+let i = 0
+
+const rewriteNum = document.getElementById('rewriteNum')
+rewriteNum.textContent = i
+
+function rewriteCntUp() {
+
+    i++
 
     const isFizz = i % 3 === 0    // 3の倍数
     const isBuzz = i % 5 === 0    // 5の倍数
 
+    // fizz、buzz、fizzbuzz以外の場合
+    let fizzBuzzTxt = i
+
     // 3と5の公倍数
     if (isFizz && isBuzz) {
-        console.log('fizzbuzz')
+        fizzBuzzTxt = 'fizzbuzz'
 
     } else if (isFizz) {
-        console.log('fizz')
+        fizzBuzzTxt = 'fizz'
 
     } else if (isBuzz) {
-        console.log('buss')
-
-    } else {
-        console.log(i)
+        fizzBuzzTxt = 'buss'
     }
 
+    rewriteNum.textContent = fizzBuzzTxt
 }
