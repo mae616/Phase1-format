60434eee8dd507c9cf75fa4ca096c4fac9b2f45b
mae616 <oya2.mi@hotmail.co.jp>
2021-09-08 10:03:10 +0900

    斎藤さんゲーム 追加チャレンジ問題 解答

--- js-easy/saitosan/main.js
+++ js-easy/saitosan/main.js
@@ -1,5 +1,14 @@
 const setBtn = function () {
 
-    Math.floor(Math.random() * 10) < 5 ?
-        alert('ぺっぺぺー') : alert('斎藤さんだぞ！');
+    const word1 = 'ぺっぺぺー';
+    const word2 = '斎藤さんだぞ！';
+
+    if (Math.floor(Math.random() * 10) < 5) {
+        confirm(word1) ?
+            alert(word1) : alert(word2);
+
+    } else {
+        confirm(word2) ?
+            alert(word2) : alert(word1);
+    }
 }
