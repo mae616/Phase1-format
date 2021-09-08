0e2e3ac44f10059cba10a2b8d7242574397c1cc8
mae616 <oya2.mi@hotmail.co.jp>
2021-09-08 09:39:51 +0900

    斎藤さんゲーム

---
+++ js-easy/saitosan/main.js
@@ -0,0 +1,10 @@
+const setBtn = function () {
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random < 5) {
+        alert('ぺっぺぺー');
+    } else {
+        alert('斎藤さんだぞ！')
+    }
+}
