7180af6c03b4f689f22eca419e0bc7be5f12c964
mae616 <oya2.mi@hotmail.co.jp>
2021-09-09 13:37:38 +0900

    サイコロアプリ チャレンジ問題 解答

--- js-easy/dice/main.js
+++ js-easy/dice/main.js
@@ -1,4 +1,35 @@
-let isRolling = false;  // サイコロ実行中
+// 解答例
+// const startBtn = document.getElementById('diceBtn');
+// const body = document.querySelector('body');
+// const dice = document.createElement('img');
+// dice.style.width = '100px';
+// dice.style.height = '100px';
+
+// let diceNum = './img/saikoro1.png';
+// dice.setAttribute('src', diceNum);
+// body.appendChild(dice);
+
+// let timer;
+// let count = 0;
+
+// startBtn.addEventListener('click', function () {
+//     clearInterval(timer);
+//     timer = setInterval('random()', 100);
+
+//     setTimeout(() => {
+//         clearInterval(timer);
+//         count = 0;
+//     }, 3000);
+// });
+
+// const random = function () {
+//     console.log(count++);
+//     diceNum = `./img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
+//     dice.setAttribute('src', diceNum);
+// };
+
+// -----------------------------
+
 
 // サイコロイメージをドキュメントに追加
 const diceBtn = document.getElementById('diceBtn');
@@ -13,18 +44,22 @@ diceImg.style.height = '100px';
 let diceNum = './img/saikoro1.png';
 diceImg.setAttribute('src', diceNum);
 
+let timer;
+let timeout;
+let count = 0;
 function randomDice() {
+    console.log(count++);
     diceNum = `./img/saikoro${Math.floor(Math.random() * 6) + 1}.png`;
     diceImg.setAttribute('src', diceNum);
 }
 
 diceBtn.addEventListener('click', () => {
-    if (!isRolling) {
-        isRolling = true;
-        let timer = setInterval(randomDice, 100)
-        setTimeout(function () {
-            clearInterval(timer);
-            isRolling = false;
-        }, 3000);
-    }
+    clearInterval(timer);
+    clearTimeout(timeout);
+    count = 0;
+    timer = setInterval(randomDice, 100)
+    timeout = setTimeout(function () {
+        clearInterval(timer);
+        count = 0;
+    }, 3000);
 });
