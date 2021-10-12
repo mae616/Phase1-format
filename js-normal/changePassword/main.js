6f15c25e593f812dd51e7e1428404511a1ddc281
mae616 <oya2.mi@hotmail.co.jp>
2021-10-12 21:05:45 +0900

    パスワードチェンジ 追加チャレンジ問題

--- js-normal/changePassword/main.js
+++ js-normal/changePassword/main.js
@@ -12,8 +12,14 @@ setPassword.addEventListener('click', function () {
         alert('古いパスワードが間違っています！');
     } else if (password === newPassword) {
         alert('同じパスワードはつかえません！');
-    } else if (newPassword.length < 8) {
-        alert('8文字以上にしてください。');
+        // } else if (newPassword.length < 8) {
+        //     alert('8文字以上にしてください。');
+        // } else if (!/^\d{3}-\d{4}$/.test(newPassword)) {
+        //     alert('xxx-yyyyの形式で入力してください');
+        // } else if (/(.)\1+/.test(newPassword)) {
+        //     alert('同じ文字を連続してはつかえません！');
+    } else if (/abc/.test(newPassword)) {
+        alert('ルールに反しているものはつかえません！');
     } else {
         password = newPassword;
         alert(`新しいパスワードは${password}です`);
