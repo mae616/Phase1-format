ee5e3d1f98bc9473f3fefeb170b6fd6d4b84ef01
mae616 <oya2.mi@hotmail.co.jp>
2021-10-13 20:22:39 +0900

    ToDo

---
+++ js-hard/ToDo/main.js
@@ -0,0 +1,27 @@
+const addTaskBtn = document.getElementsByClassName('add-btn');
+
+addTaskBtn[0].addEventListener('click', () => {
+    const todoArea = document.getElementById('todo');
+    const addTaskText = document.getElementById('add-area').value;
+    // タスクの追加
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = addTaskText;
+    li.appendChild(p);
+
+    const finishBtn = document.createElement('button');
+    finishBtn.textContent = '完了';
+    // イベントリスナーの登録
+    finishBtn.addEventListener('click', function finishTask(e) {
+        // イベントリスナーの削除
+        e.target.removeEventListener('click', finishTask);
+        // タスクの削除
+        const removeChild = e.target.parentNode;
+        const parentNode = removeChild.parentNode;
+
+        parentNode.removeChild(removeChild);
+    });
+    li.appendChild(finishBtn);
+
+    todoArea.appendChild(li);
+});
